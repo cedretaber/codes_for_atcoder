9c52f22831b0f140ea78b8f9888d85c030386c93
cedretaber <cedretaber@gmail.com>
2019-12-02 09:17:00 +0900

    Solve kupc2017_c.

--- other/kupc/2017/c.d
+++ other/kupc/2017/c.d
@@ -2,5 +2,18 @@ import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.
 
 void main()
 {
-    
+    auto A = readln.chomp.to!int;
+    auto S = readln.chomp.to!(char[]);
+    foreach (_; 0..1000) {
+        foreach_reverse (i; 1..S.length) {
+            while (S[i-1] + A <= 'z' && S[i] >= 'a') {
+                S[i-1] += A;
+                --S[i];
+            }
+            if (S[i] == 'a'-1) S[i] = 0;
+        }
+    }
+    char[] r;
+    foreach (c; S) if (c != 0) r ~= c;
+    writeln(r);
 }
---
+++ other/kupc/2017/c_mk.rb
@@ -0,0 +1,2 @@
+puts [*2..10].sample
+puts 1000.times.map { [*?a..?z].sample }.join
