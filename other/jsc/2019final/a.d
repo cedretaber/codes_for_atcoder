6919f932aee1155883547a18151a3713341016d6
cedretaber <cedretaber@gmail.com>
2019-12-05 05:02:27 +0900

    Solve JSC2019final_A.

---
+++ other/jsc/2019final/a.d
@@ -0,0 +1,30 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric;
+
+alias IJ = Tuple!(int, "i", int, "j", bool, "a");
+
+void main()
+{
+    auto nm = readln.split.to!(int[]);
+    auto N = nm[0];
+    auto M = nm[1];
+    auto as = readln.split.to!(int[]);
+    auto bs = readln.split.to!(int[]);
+
+    auto memo = new IJ[](2*10^^6);
+    int c;
+    foreach (i; 0..N) {
+        foreach (j; 0..M) {
+            if (c == 2*10^^6) goto ng;
+            auto w = as[i] + bs[j];
+            if (memo[w].a) {
+                auto ij = memo[w];
+                writefln("%d %d %d %d", ij.i, ij.j, i, j);
+                return;
+            }
+            ++c;
+            memo[w] = IJ(i, j, true);
+        }
+    }
+    ng:
+    writeln("-1");
+}
