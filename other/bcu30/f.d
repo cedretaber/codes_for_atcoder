46ea846e7615ce2507814af28baaa7df421c55dc
cedretaber <cedretaber@gmail.com>
2019-12-20 04:47:29 +0900

    Solve BCU30 f.

--- other/bcu30/f.d
+++ other/bcu30/f.d
@@ -1,10 +1,9 @@
 import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric;
 
-
 long P = 10L^^9+7;
-long[10^^5+50] F, RF;
 
-long pow(long x, long n) {
+long pow(long x, long n)
+{
     long y = 1;
     while (n) {
         if (n%2 == 1) y = (y * x) % P;
@@ -14,37 +13,16 @@ long pow(long x, long n) {
     return y;
 }
 
-/**
-  * P を法とする階乗、逆元を予め計算しておく。
-  */
-void init()
+long to_inv(long x)
 {
-    F[0] = F[1] = 1;
-    foreach (i, ref x; F[2..$]) x = (F[i+1] * (i+2)) % P;
-
-    {
-        RF[$-1] = 1;
-        auto x = F[$-1];
-        auto k = P-2;
-        while (k) {
-            if (k%2 == 1) RF[$-1] = (RF[$-1] * x) % P;
-            x = x^^2 % P;
-            k /= 2;
-        }
+    long n = 1;
+    long k = P-2;
+    while (k) {
+        if (k%2 == 1) n = (n * x) % P;
+        x = x^^2 % P;
+        k /= 2;
     }
-    foreach_reverse(i, ref x; RF[0..$-1]) x = (RF[i+1] * (i+1)) % P;
-}
-
-long comb(N)(N n, N k)
-{
-    if (k > n) return 0;
-    auto n_b = F[n];    // n!
-    auto nk_b = RF[n-k]; // 1 / (n-k)!
-    auto k_b = RF[k];    // 1 / k!
-
-    auto nk_b_k_b = (nk_b * k_b) % P; // 1 / (n-k)!k!
-
-    return (n_b * nk_b_k_b) % P;  // n! / (n-k)!k!
+    return n;
 }
 
 void main()
@@ -52,7 +30,31 @@ void main()
     auto N = readln.chomp.to!int;
     auto as = readln.split.to!(long[]);
 
-    auto cs = new long[](N);
-    cs[0] = as[0];
-    foreach (i; 1..N) cs[i] = (cs[i-1] * as[i]) % P;
+    if (N == 1) {
+        writeln(as[0]);
+        return;
+    } else if (N == 2) {
+        writeln((as[0] + as[1] + as[0] * as[1]) % P);
+        return;
+    }
+    long i2 = 2.to_inv();
+    long a = (as[0] * pow(2, N-2)) % P;
+    long ap = a;
+    foreach (i; 1..N-1) {
+        ap = ((ap * as[i]) % P * i2) % P;
+        a = (a + ap) % P;
+    }
+    a = (a + ap * as[N-1]) % P;
+    long s = a;
+    foreach (i; 1..N-1) {
+        a = (a * as[i-1].to_inv()) % P;
+        if (i == 1) {
+            a = (a + P - pow(2, N-2));
+        } else {
+            a = (a + P - pow(2, N-3));
+            a = (a * 2) % P;
+        }
+        s = (s + a) % P;
+    }
+    writeln((s + as[N-1] * pow(2, N-2)) % P);
 }
