36a31283bba12b90c9e379001f289bb2e6204969
cedretaber <cedretaber@gmail.com>
2019-12-30 08:22:50 +0900

    Solve ABC149 e.

--- abc/149/e.d
+++ abc/149/e.d
@@ -6,36 +6,54 @@ void main()
     auto N = nm[0];
     auto M = nm[1];
     auto as = readln.split.to!(long[]);
-    sort!"a > b"(as);
-    auto cs = new long[](N);
-    foreach_reverse (i; 0..N) {
-        if (i+1 < N) cs[i] = cs[i+1];
-        cs[i] += as[i];
-    }
+    sort(as);
 
-    long r;
-    foreach (i; 0..N) {
-        auto k = (N-i)*2 - 1;
-        if (M >= k) {
-            r += as[i] * (k+1);
-            if (i+1 < N) r += cs[i+1] * 2;
-            M -= k;
+    auto cs = new long[](10^^5+1);
+    foreach (a; as) {
+        cs[a] += 1;
+    }
+    foreach_reverse (i; 0..10^^5) {
+        cs[i] += cs[i+1];
+    }
+    long x, y = 10^^5*2+1;
+    while (x+1 < y) {
+        auto m = (x+y)/2;
+        long c;
+        foreach (i; 0..N) {
+            auto n = m - as[i];
+            if (n < 0) n = 0;
+            if (n > 10^^5) continue;
+            c += cs[n];
+        }
+        if (c >= M) {
+            x = m;
         } else {
-            if (M == 0) break;
-            r += as[i]*2;
-            M -= 1;
-            foreach (j; i+1..N) {
-                if (M == 0) {
-                    break;
-                } else if (M == 1) {
-                    r += as[i] + as[j];
-                    M = 0;
-                } else {
-                    r += (as[i] + as[j]) * 2;
-                    M -= 2;
-                }
-            }
+            y = m;
         }
     }
-    writeln(r);
+    auto ds = new long[](N);
+    foreach_reverse (i; 0..N) {
+        if (i+1 < N) ds[i] = ds[i+1];
+        ds[i] += as[i];
+    }
+    long r, c;
+    foreach (i; 0..N) {
+        if (as[i] + as[0] >= x) {
+            r += as[i] * N + ds[0];
+            c += N;
+        } else if (as[i] + as[N-1] >= x) {
+            long a, b = N-1;
+            while (a+1 < b) {
+                auto m = (a+b)/2;
+                if (as[i] + as[m] >= x) {
+                    b = m;
+                } else {
+                    a = m;
+                }
+            }
+            r += as[i] * (N-b) + ds[b];
+            c += N-b;
+        }
+    }
+    writeln(r - x*(c-M));
 }
---
+++ abc/149/e_mk.rb
@@ -0,0 +1,2 @@
+puts "#{10**5} #{rand(1..10**10)}"
+puts 100000.times.map { rand(1..10**5) }.join(" ")
