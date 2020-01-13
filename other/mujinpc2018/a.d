0b653469237a63fbb2e02a28057c9e21c5bb16fc
cedretaber <cedretaber@gmail.com>
2020-01-14 03:56:15 +0900

    Chore.

--- other/mujinpc2018/a.d
+++
@@ -1,7 +0,0 @@
-import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons;
-
-void main()
-{
-    auto S = readln.chomp;
-    writeln(S.length > 4 && S[0..5] == "MUJIN" ? "Yes" : "No");
-}
