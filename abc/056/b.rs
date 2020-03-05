da37e22aff3315c7f180b530e90f41a4dec63d41
cedretaber <cedretaber@gmail.com>
2020-03-05 20:29:28 +0900

    Solve ABC056 a to c with Rust.

---
+++ abc/056/b.rs
@@ -0,0 +1,37 @@
+macro_rules! get {
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+use std::cmp;
+
+fn main() {
+    let (w, aa, bb) = get!(i32, i32, i32);
+    let (a, b) = if aa < bb { (aa, bb) } else { (bb, aa) };
+    println!("{}", cmp::max(0, b - (a+w)));
+}
---
+++ abc/056/c.rs
@@ -0,0 +1,40 @@
+macro_rules! get {
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let mut x = get!(i64);
+    for i in 1..1000000000 {
+        x -= i;
+        if x <= 0 {
+            println!("{}", i);
+            return;
+        }
+    }
+}
