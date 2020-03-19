9e457812e98225b5b5c9e5ca39d820001a578c4b
cedretaber <cedretaber@gmail.com>
2020-03-19 22:34:32 +0900

    Solve ABC058 d again.

--- abc/058/d.rs
+++ abc/058/d.rs
@@ -32,8 +32,13 @@ const P: i64 = 1000000007;
 
 fn main() {
     let (n, m) = get!(i64, i64);
-    let d = (n-1) * (m-1) % P;
 
     let xs = get!(String).split_whitespace().map(|n| n.parse::<i64>().unwrap()).collect::<Vec<_>>();
     let ys = get!(String).split_whitespace().map(|n| n.parse::<i64>().unwrap()).collect::<Vec<_>>();
+
+    let mut x = 0;
+    let mut y = 0;
+    for i in 0..n-1 { x = (x + (xs[(i+1) as usize] - xs[i as usize]) * (i+1) % P * (n-i-1)) % P; }
+    for i in 0..m-1 { y = (y + (ys[(i+1) as usize] - ys[i as usize]) * (i+1) % P * (m-i-1)) % P; }
+    println!("{}", x * y % P);
 }
