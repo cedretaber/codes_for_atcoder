d34c33bc1c073e3a0a9cf8de124f488c7dbd58c9
cedretaber <cedretaber@gmail.com>
2020-03-30 01:42:44 +0900

    Solve tenkaichi2015yo a.

---
+++ other/tenkaichi/2015yoa/a.d
@@ -0,0 +1,25 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric;
+
+void main()
+{
+    writeln("654231");
+}
+
+/*
+
+6
+5423
+   1
+
+65
+ 41
+  23
+
+65
+ 413
+   2
+
+651
+  423
+
+*/
