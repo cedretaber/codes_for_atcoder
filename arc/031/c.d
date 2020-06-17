77a58644eba20d24ac8e6ccf9e2fa41425fd8586
cedretaber <cedretaber@gmail.com>
2020-06-18 00:13:37 +0900

    Solve ARC031 c.

--- arc/031/c.d
+++ arc/031/c.d
@@ -1,36 +1,46 @@
 import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.bigint;
 
-/// [..), 1-origin
-struct BITree(alias _fun, alias init, T)
-if (is(typeof(init) : T))
+struct SegTree(alias fun, alias E, T)
+if (is(typeof(E) : T))
 {
     import std.functional : binaryFun;
-    alias fun = binaryFun!_fun;
+    alias OP = binaryFun!fun;
 
     ///
     this(size_t n, T[] ts) {
-        this.n = n;
-        this.tree.length = n+1;
-        foreach (ref e; this.tree) e = init;
-        foreach (i, t; ts) this.put(i+1, t);
+        this.n = 1;
+        while (this.n < n) this.n *= 2;
+        this.tree.length = this.n * 2 - 1;
+        foreach (ref e; this.tree) e = E;
+        foreach (i, e; ts) this.update(i, e);
     }
 
     ///
-    void put(size_t i, T e) {
-        while (i <= this.n) {
-            this.tree[i] = fun(this.tree[i], e);
-            i += i & -i;
-        }
-    }
-
-    ///
-    T query(size_t i) {
-        auto r = init;
+    void replace(size_t i, T e) {
+        i += this.n - 1;
+        this.tree[i] = e;
         while (i > 0) {
-            r = fun(r, this.tree[i]);
-            i -= i & -i;
+            i = (i-1) / 2;
+            this.tree[i] = OP(this.tree[i*2+1], this.tree[i*2+2]);
         }
-        return r;
+    }
+
+    ///
+    void update(size_t i, T e) {
+        replace(i, OP(e, tree[i + this.n - 1]));
+    }
+
+    ///
+    T query(size_t a, size_t b) {
+        T impl(size_t i, size_t l, size_t r) {
+            if (r <= a || b <= l) return E;
+            if (a <= l && r <= b) return this.tree[i];
+            return OP(
+                impl(i*2+1, l, (l+r)/2),
+                impl(i*2+2, (l+r)/2, r)
+            );
+        }
+        return impl(0, 0, this.n);
     }
 
 private:
@@ -39,26 +49,43 @@ private:
 }
 
 ///
-BITree!(fun, init, T) bitree(alias fun, alias init, T)(size_t n, T[] ts = [])
+auto seg_tree(alias f, alias E, T)(size_t n, T[] arr = [])
 {
-    return BITree!(fun, init, T)(n, ts);
+    return SegTree!(f, E, T)(n, arr);
 }
 
 ///
-BITree!(fun, init, T) bitree(alias fun, alias init, T)(T[] ts)
+auto seg_tree(alias f, alias E, T)(T[] arr)
 {
-    return BITree!(fun, init, T)(ts.length, ts);
+    return SegTree!(f, E, T)(arr.length, arr);
+}
+
+auto sum_seg_tree(T)(size_t n, T[] arr = [])
+{
+    return seg_tree!("a + b", 0, T)(n, arr);
+}
+
+auto sum_seg_tree(T)(T[] arr)
+{
+    return sum_seg_tree!T(arr.length, arr);
 }
 
 void main()
 {
     auto N = readln.chomp.to!int;
-    auto bit = bitree!("a + b", 0L, long)(N);
+    auto ii = new int[](N+1);
+    ii[] = -1;
+    auto segt = sum_seg_tree!int(N);
+    foreach (i, b; readln.split.to!(int[])) {
+        ii[b] = i.to!int;
+        segt.replace(i, 1);
+    }
     long r;
-    foreach (ii, b; readln.split.to!(long[])) {
-        auto i = ii.to!long;
-        r += i - bit.query(i+1);
-        bit.put(i+1, 1);
+    foreach (b; 0..N+1) if (ii[b] != -1) {
+        auto i = ii[b];
+        segt.replace(i, 0);
+        if (i == 0 || i == N-1) continue;
+        r += min(segt.query(0, i), segt.query(i+1, N));
     }
     writeln(r);
 }
