b6dfacc9dbda9f20fa174f635e5497d355f7e09b
cedretaber <cedretaber@gmail.com>
2020-06-30 04:07:18 +0900

    Solve CodeFestival2017qb c.

--- other/codefestival/2017qb/c.d
+++ other/codefestival/2017qb/c.d
@@ -2,7 +2,7 @@ import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.
 
 void main()
 {
-    auto nm = readln.split.to!(int[]);
+    auto nm = readln.split.to!(long[]);
     auto N = nm[0];
     auto M = nm[1];
     int[][] G;
@@ -15,9 +15,33 @@ void main()
         G[B] ~= A;
     }
 
-    int r;
-    auto memo = new bool[](N);
-    void walk(int i, int[] as, int[] bs, int[] cs) {
-        int nbs = [];
+    auto es = new int[](N);
+    long x, y, z;
+    void walk(int i, int p, int c) {
+        c = c == 1 ? 2 : 1;
+        foreach (j; G[i]) if (j != p) {
+            if (z) return;
+            if (es[j] == 0) {
+                if (c == 1) {
+                    ++x;
+                } else {
+                    ++y;
+                }
+                es[j] = c;
+                walk(j, i, c);
+            } else if ((es[j] == 1 && c == 2) || es[j] == 2 && c == 1) {
+                z = 1;
+                return;
+            }
+        }
+    }
+    es[0] = 1;
+    ++x;
+    walk(0, -1, 1);
+
+    if (z) {
+        writeln(N * (N-1) / 2 - M);
+    } else {
+        writeln(x * y - M);
     }
 }
