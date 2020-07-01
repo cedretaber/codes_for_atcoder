5373dc1b66ea79381311ad2947622f8f0ed9eaf2
cedretaber <cedretaber@gmail.com>
2020-07-02 00:44:59 +0900

    Solve ARC094 e.

--- arc/094/e.d
+++ arc/094/e.d
@@ -1,18 +1,31 @@
 import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons;
 
-int[10^^5 * 2] AS, BS, SS; 
-
 void main()
 {
     auto N = readln.chomp.to!int;
-    long ret;
+
+    long s;
+    bool neq;
+    long[] AS, BS;
     foreach (_; 0..N) {
-        auto ab = readln.split.to!(int[]);
+        auto ab = readln.split.to!(long[]);
         auto A = ab[0];
         auto B = ab[1];
-
-        if (A <= B) {
-
+        s += A;
+        if (A >= B) {
+            AS ~= A;
+            BS ~= B;
+        } else {
+            neq = true;
         }
     }
+    if (!neq) {
+        writeln(0);
+        return;
+    }
+    long min_b = long.max;
+    foreach (i; 0..AS.length) {
+        if (AS[i] != BS[i] && BS[i] < min_b) min_b = BS[i];
+    }
+    writeln(s - min_b);
 }
