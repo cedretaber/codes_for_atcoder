eedb6fee0973a4a0cee513dd3ddc37a9a0b8de08
cedretaber <cedretaber@gmail.com>
2020-07-04 03:24:56 +0900

    Modify ARC091 e.

--- arc/091/e.d
+++ arc/091/e.d
@@ -25,103 +25,17 @@ void main()
         writeln(-1);
         return;
     }
-    auto r = N-A;
-    int x;
-    while (r-A + x+1 > B-1) {
-        r -= A;
-        x += 1;
-    }
-    int d;
-    if (r+x > B-1) {
-        d = r + x+1 - (B-1);
-        r -= d;
-    }
+    auto x = (N-A)/(B-1);
+    auto r = (N-A)%(B-1);
     int[] res;
     int i = N;
-    if (r) {
-        res ~= (N-r+1).iota(N+1).array.reverse;
-        i -= r;
+    foreach (_b; 0..B-1) {
+        auto n = x + (r > 0 ? 1 : 0);
+        foreach_reverse (d; 0..n) res ~= i-d;
+        i -= n;
+        --r;
     }
-    if (d) {
-        foreach_reverse (j; 0..d) res ~= i-j;
-        i -= d;
-    }
-    foreach (_x; 0..x+1) {
-        foreach_reverse (j; 0..A) res ~= i-j;
-        i -= A;
-    }
+    foreach_reverse (d; 0..A) res ~= i-d;
     if (rev) res = res.reverse.array;
     writeln(res.to!(string[]).join(" "));
 }
-
-/*
-
-r + x+1 = B-1 + d
-
-10 2 7
-10 9 8 7 5 6 3 4 1 2
-
-9 3 3
-7 8 9 4 5 6 1 2 3
-
-9 3 7
-9 8 7 6 5 4 1 2 3
-
-9 2 5
-9 7 8 5 6 3 4 1 2
-
-9 3 4
-9 7 8 4 5 6 1 2 3
-
-10 4 4
-5 1 6 2 7 3 10 9 8 4
-
-8 5 3
-1 6 2 3 4 8 7 5
-
-3 1 3
-3 2 1
-
-3 3 1
-1 2 3
-
-3 2 2
-1 3 2
-
-4 1 5
-5 4 3 2 1
-
-4 2 2
-3 1 4 2
-
-4 2 3
-1 4 3 2
-
-
-
-7 4 2
-1 5 2 6 3 7 4
-
-
-5 3 2
-1 4 2 5 3
-
-7 4 2
-1 5 2 6 3 7 4
-
-7 4 3
-1 2 5 3 7 6 4
-
-7 4 4
-1 2 3 7 6 5 4
-
-7 5 1
-1 2 3 6 4 7 5
-
-5 2 3
-3 1 5 4 2
-
-5 1 4
-5 4 3 2 1
-
-*/
