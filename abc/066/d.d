b3a677700ddf695eb1e31784b07fa4375b71fa3f
cedretaber <cedretaber@gmail.com>
2020-07-06 01:53:22 +0900

    Change filenames.

--- abc/066/d.d
+++ abc/066/d.d
@@ -1,49 +1,81 @@
-import std.stdio, std.algorithm, std.conv, std.array, std.string;
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
 
-long N = 10 ^^ 9 + 7;
-int[10 ^^ 5 + 1] CS;
+long P = 10^^9+7;
+long[10^^5+50] F, RF;
 
-long comb(long r, long n)
+long pow(long x, long n) {
+    long y = 1;
+    while (n) {
+        if (n%2 == 1) y = (y * x) % P;
+        x = x^^2 % P;
+        n /= 2;
+    }
+    return y;
+}
+
+long inv(long x)
 {
-    long p = 1;
+    return pow(x, P-2);
+}
 
-    foreach (i; 0..r)
-        p = (p * (n - i) / (i + 1)) % N;
+void init()
+{
+    F[0] = F[1] = 1;
+    foreach (i, ref x; F[2..$]) x = (F[i+1] * (i+2)) % P;
 
-    return p;
+    {
+        RF[$-1] = 1;
+        auto x = F[$-1];
+        auto k = P-2;
+        while (k) {
+            if (k%2 == 1) RF[$-1] = (RF[$-1] * x) % P;
+            x = x^^2 % P;
+            k /= 2;
+        }
+    }
+    foreach_reverse(i, ref x; RF[0..$-1]) x = (RF[i+1] * (i+1)) % P;
+}
+
+long comb(N)(N n, N k)
+{
+    if (k > n) return 0;
+    auto n_b = F[n];    // n!
+    auto nk_b = RF[n-k]; // 1 / (n-k)!
+    auto k_b = RF[k];    // 1 / k!
+
+    auto nk_b_k_b = (nk_b * k_b) % P; // 1 / (n-k)!k!
+
+    return (n_b * nk_b_k_b) % P;  // n! / (n-k)!k!
 }
 
 void main()
 {
-    auto n = readln.chomp.to!int;
-    auto as = readln.split.to!(int[]);
-
-    int dupl;
-    int before, after;
-
-    foreach (i; 0..n + 1) {
-        if (CS[as[i]]) {
-            dupl = as[i];
-            break;
+    init();
+    auto N = readln.chomp.to!long;
+    auto ii = new long[](N+1);
+    ii[] = -1;
+    long l, r;
+    foreach (i, a; readln.split.to!(long[])) {
+        if (ii[a] == -1) {
+            ii[a] = i;
+        } else {
+            l = ii[a];
+            r = i;
         }
-        ++CS[as[i]];
     }
-    int cnt;
-    foreach (i; 0..n + 1) {
-        if (as[i] == dupl) {
-            
+    foreach (k; 1..N+2) {
+        long c;
+        (c += comb(N-1, k)) %= P;
+        if (k == 1) {
+            writeln(c+1);
+            continue;
         }
-        if (CS[as[i]] == 2) {
-            
-        }
-        ++cnt;
-        ++CS[as[i]];
-    }
-    
-    foreach (i; 0..n) {
-        long sum;
-        foreach (j; 0..(n - i)) {
-
-        }
+        (c += comb(N-1, k-2)) %= P;
+        (c += comb(l + N-r, k-1)) %= P;
+        long d = comb(N-1, k-1);
+        (d += P - comb(l + N-r, k-1)) %= P;
+        (d *= 2) %= P;
+        (c += d) %= P;
+        writeln(c);
     }
 }
