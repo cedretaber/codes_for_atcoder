1052dbc9172cdcc3a208d8aa6eb5fb2f6fd63363
cedretaber <cedretaber@gmail.com>
2020-09-26 17:34:48 +0900

    Solve ABC059 d again.

---
+++ abc/059/d2.d
@@ -0,0 +1,72 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+            args[0] = readln.split.to!Arg;
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    long X, Y; get(X, Y);
+    writeln(abs(X - Y) <= 1 ? "Brown" : "Alice");
+}
+
+/*
+
+  0 1 2 3 4 5 6 7 8 9
+0 B B A A A A A
+1 B B B A A A A
+2 A B B B A A A
+3 A A B B B A A A
+4 A A A B B B A A A
+5
+6
+7
+8
+9
+
+
+*/
