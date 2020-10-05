04f91eacc65e4497965b2337cdc232389386ac0b
cedretaber <cedretaber@gmail.com>
2020-10-06 03:21:13 +0900

    Use numbered parameter.

--- other/joi/2013yo/c.rb
+++ other/joi/2013yo/c.rb
@@ -1,7 +1,7 @@
 n = gets.to_i
 s = gets.chomp.chars
-rs = Array.new(100) { |i| Regexp.new(s.join ".{#{i}}") }
+rs = Array.new(100) { Regexp.new(s * ".{#{_1}}") }
 puts Array.new(n) {
   t = gets.chomp
-  rs.any? { |r| r =~ t } ? 1 : 0
+  rs.any? { _1 =~ t } ? 1 : 0
 }.sum
