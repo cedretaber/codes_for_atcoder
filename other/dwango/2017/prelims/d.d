0e067bb4355d29e322ab260ed3b9793bcd04df34
cedretaber <cedretaber@gmail.com>
2020-10-09 11:00:27 +0900

    Solve dwango2017q d.

--- other/dwango/2017/prelims/d.d
+++ other/dwango/2017/prelims/d.d
@@ -1,18 +1,72 @@
-import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric;
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
 
-alias X = Tuple!(int, "i", int, "x");
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
 
 void main()
 {
-    auto nm = readln.split.to!(int[]);
-    auto N = nm[0];
-    auto M = nm[1];
+    int N, M; get(N, M);
+    int[] xs, ys; get_lines(N, xs, ys);
 
-    int[] xs, ys;
-    auto ix = new X[](N);
-
+    int h, max_h;
+    auto ss = heapify!"a > b"([1000]);
     foreach (i; 0..N) {
-        auto xy = readln.split.to!(int[]):
-        
+        if (i >= M) {
+            h -= ss.front;
+            ss.popFront();
+        }
+        h += xs[i];
+        ss.insert(xs[i] - ys[i]);
+        max_h = max(max_h, h);
     }
+    writeln(max_h);
 }
