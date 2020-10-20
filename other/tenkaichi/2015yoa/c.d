cb70f8fb12d28f94017a0576e58ca6f5677470dd
cedretaber <cedretaber@gmail.com>
2020-10-20 23:14:21 +0900

    Solve tenkaichi2015qa c.

--- other/tenkaichi/2015yoa/c.d
+++ other/tenkaichi/2015yoa/c.d
@@ -52,6 +52,206 @@ void get_lines(Args...)(size_t N, ref Args args)
     }
 }
 
+struct MFGraph(Cap)
+{
+    import std.typecons : Tuple;
+
+public:
+    this(int n)
+    {
+        _n = n;
+        g = new _edge[][](n);
+    }
+
+    int addEdge(int from, int to, Cap cap)
+    {
+        assert(0 <= from && from < _n);
+        assert(0 <= to && to < _n);
+        assert(0 <= cap);
+        int m = cast(int) pos.length;
+        pos ~= Tuple!(int, int)(from, cast(int)(g[from].length));
+        int from_id = cast(int) g[from].length;
+        int to_id = cast(int) g[to].length;
+        if (from == to)
+            to_id++;
+        g[from] ~= _edge(to, to_id, cap);
+        g[to] ~= _edge(from, from_id, 0);
+        return m;
+    }
+
+    struct Edge
+    {
+        int from, to;
+        Cap cap, flow;
+    }
+
+    Edge getEdge(int i)
+    {
+        int m = cast(int)(pos.length);
+        assert(0 <= i && i < m);
+        auto _e = g[pos[i][0]][pos[i][1]];
+        auto _re = g[_e.to][_e.rev];
+        return Edge(pos[i][0], _e.to, _e.cap + _re.cap, _re.cap);
+    }
+
+    Edge[] edges()
+    {
+        int m = cast(int)(pos.length);
+        Edge[] result;
+        foreach (i; 0 .. m)
+            result ~= getEdge(i);
+        return result;
+    }
+
+    void changeEdge(int i, Cap new_cap, Cap new_flow)
+    {
+        int m = cast(int)(pos.length);
+        assert(0 <= i && i < m);
+        assert(0 <= new_flow && new_flow <= new_cap);
+        auto _e = &g[pos[i][0]][pos[i][1]];
+        auto _re = &g[_e.to][_e.rev];
+        _e.cap = new_cap - new_flow;
+        _re.cap = new_flow;
+    }
+
+    Cap flow(int s, int t)
+    {
+        return flow(s, t, Cap.max);
+    }
+
+    Cap flow(int s, int t, Cap flow_limit)
+    {
+        import std.container : DList;
+        import std.algorithm : min;
+
+        assert(0 <= s && s < _n);
+        assert(0 <= t && t < _n);
+        assert(s != t);
+
+        auto level = new int[](_n), iter = new int[](_n);
+        DList!int que;
+
+        void bfs()
+        {
+            level[] = -1;
+            level[s] = 0;
+            que.clear();
+            que.insertBack(s);
+            while (!que.empty)
+            {
+                int v = que.front;
+                que.removeFront();
+                foreach (e; g[v])
+                {
+                    if (e.cap == 0 || level[e.to] >= 0)
+                        continue;
+                    level[e.to] = level[v] + 1;
+                    if (e.to == t)
+                        return;
+                    que.insertBack(e.to);
+                }
+            }
+        }
+
+        Cap dfs(int v, Cap up)
+        {
+            if (v == s)
+                return up;
+            Cap res = 0;
+            int level_v = level[v];
+            for (; iter[v] < cast(int)(g[v].length); iter[v]++)
+            {
+                auto i = iter[v];
+                auto e = g[v][i];
+                if (level_v <= level[e.to] || g[e.to][e.rev].cap == 0)
+                    continue;
+                Cap d = dfs(e.to, min(up - res, g[e.to][e.rev].cap));
+                if (d <= 0)
+                    continue;
+                g[v][i].cap += d;
+                g[e.to][e.rev].cap -= d;
+                res += d;
+                if (res == up)
+                    break;
+            }
+            return res;
+        }
+
+        Cap flow = 0;
+        while (flow < flow_limit)
+        {
+            bfs();
+            if (level[t] == -1)
+                break;
+            iter[] = 0;
+            while (flow < flow_limit)
+            {
+                Cap f = dfs(t, flow_limit - flow);
+                if (!f)
+                    break;
+                flow += f;
+            }
+        }
+        return flow;
+    }
+
+    bool[] minCut(int s)
+    {
+        import std.container : DList;
+
+        auto visited = new bool[](_n);
+        DList!int que;
+        que.insertBack(s);
+        while (!que.empty)
+        {
+            int p = que.front;
+            que.removeFront();
+            visited[p] = true;
+            foreach (e; g[p])
+            {
+                if (e.cap && !visited[e.to])
+                {
+                    visited[e.to] = true;
+                    que.insertBack(e.to);
+                }
+            }
+        }
+        return visited;
+    }
+
+private:
+    struct _edge
+    {
+        int to, rev;
+        Cap cap;
+    }
+
+    int _n;
+    Tuple!(int, int)[] pos;
+    _edge[][] g;
+}
+
 void main()
 {
+    int M, N; get(M, N);
+    int[][] as, bs; get_lines(M, as); get_lines(M, bs);
+    auto G = MFGraph!int(M * N + 2);
+    int res;
+    foreach (i; 0..M) foreach (j; 0..N) if (as[i][j] != bs[i][j]) {
+        ++res;
+        auto c = as[i][j];
+        if (c == 0) {
+            G.addEdge(0, i*N+j+1, 1);
+            static foreach (d; [[1,0], [0,1], [-1,0], [0,-1]]) {{
+                auto ii = i+d[0];
+                auto jj = j+d[1];
+                if (0 <= ii && ii < M && 0 <= jj && jj < N && as[ii][jj] == 1) {
+                    G.addEdge(i*N+j+1, ii*N+jj+1, 1);
+                }
+            }}
+        } else {
+            G.addEdge(i*N+j+1, M*N+1, 1);
+        }
+    }
+    writeln(res - G.flow(0, N*M+1));
 }
