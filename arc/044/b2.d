e3ef16b965ea4d6d5411e6435f099a4f136805d4
cedretaber <cedretaber@gmail.com>
2020-10-30 01:45:54 +0900

    Solve ARC044 b again.

---
+++ arc/044/b2.d
@@ -0,0 +1,87 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+enum P = 10L^^9+7;
+
+long pow(long x, long n) {
+    long y = 1;
+    while (n) {
+        if (n%2 == 1) y = (y * x) % P;
+        x = x^^2 % P;
+        n /= 2;
+    }
+    return y;
+}
+
+void main()
+{
+    int N; get(N);
+    auto es = new long[](N);
+    int max_a;
+    foreach (i, aa; readln.split) {
+        auto a = aa.to!int;
+        if ((i == 0 && a != 0) || (i != 0 && a == 0)) {
+            writeln(0);
+            return;
+        }
+        max_a = max(max_a, a+1);
+        ++es[a];
+    }
+    long r = 1;
+    foreach (i; 1..max_a) {
+        (r *= pow((pow(2, es[i-1]) - 1), es[i])) %= P;
+        (r *= pow(2, es[i] * (es[i]-1) / 2)) %= P;
+    }
+    writeln(r);
+}
