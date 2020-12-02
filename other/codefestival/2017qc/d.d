84f33a8cf07824e2f966e16cacb3372d2ab47cba
cedretaber <cedretaber@gmail.com>
2020-12-02 22:24:25 +0900

    Solve CodeFestival2017qc d.

---
+++ other/codefestival/2017qc/d.d
@@ -0,0 +1,86 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    string S; get(S);
+
+    uint[] AS = [0];
+    foreach (i, c; S) {
+        AS ~= AS[$-1] ^ (1U<<(c - 'a'));
+    }
+    AS = AS[1..$];
+
+    if (AS[$-1] == 0) {
+        writeln(1);
+        return;
+    }
+
+    auto DP = new int[](2^^26);
+    DP[] = int.max/3;
+    DP[0] = 0;
+
+    uint[27] xs;
+    foreach (i; 1..27) xs[i] = 1U << (i-1);
+
+    foreach (i; 0..S.length) {
+        auto a = AS[i];
+        auto min_x = int.max / 3;
+        foreach (x; xs) {
+            min_x = min(min_x, DP[a ^ x]);
+        }
+        DP[a] = min(DP[a], min_x + 1);
+    }
+    writeln(DP[AS[$-1]]);
+}
