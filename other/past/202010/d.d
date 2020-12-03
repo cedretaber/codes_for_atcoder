37c8c51b6e5cb0d911362ad45b3b193d8783735e
cedretaber <cedretaber@gmail.com>
2020-12-03 20:29:36 +0900

    Solve PAST2010 d.

---
+++ other/past/202010/d.d
@@ -0,0 +1,80 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    int N; get(N);
+    char[] S; get(S);
+    int[] ss = [0];
+    int s, l = -1, r;
+    foreach (c; S) {
+        if (c == '#') {
+            if (l == -1) {
+                l = s;
+            } else {
+                ss ~= s;
+            }
+            s = 0;
+        } else {
+            ++s;
+        }
+    }
+    r = s;
+    sort!"a > b"(ss);
+    if (l + r >= ss[0]) {
+        writeln(l, " ", r);
+    } else {
+        writeln(l, " ", ss[0] - l);
+    }
+}
