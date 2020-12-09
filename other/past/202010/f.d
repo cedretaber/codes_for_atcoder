3f16c0d8dc7f77e9238697af8f40a1997e65ae77
cedretaber <cedretaber@gmail.com>
2020-12-09 20:54:54 +0900

    Solve PAST202010 f.

---
+++ other/past/202010/f.d
@@ -0,0 +1,74 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    int N, K; get(N, K);
+    int[string] wm;
+    foreach (_; 0..N) {
+        string S; get(S);
+        ++wm[S];
+    }
+
+    alias W = Tuple!(string, "w", int, "c");
+    W[] ws;
+    foreach (k, v; wm) ws ~= W(k, v);
+    sort!"a.c > b.c"(ws);
+    --K;
+    if ((K == 0 || ws[K].c != ws[K-1].c) && (K == ws.length || ws[K].c != ws[K+1].c)) {
+        writeln(ws[K].w);
+    } else {
+        writeln("AMBIGUOUS");
+    }
+}
