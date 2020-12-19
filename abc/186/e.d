3ba42917eeb57f2f129624fb5db91aeb966edcc6
cedretaber <cedretaber@gmail.com>
2020-12-20 02:35:49 +0900

    Solve ABC186 e.

--- abc/186/e.d
+++ abc/186/e.d
@@ -52,81 +52,36 @@ void get_lines(Args...)(size_t N, ref Args args)
     }
 }
 
-Tuple!(long, long, long) ext_gcd(long a, long b)
+long modinv(long a, long m)
 {
-    if (b == 0) {
-        return tuple(1L, 0L, a);
+    long b = m, u = 1, v = 0;
+    while (b) {
+        auto t = a / b;
+        a -= t * b; swap(a, b);
+        u -= t * v; swap(u, v);
     }
-    auto res = ext_gcd(b, a % b);
-    auto y = res[0];
-    auto x = res[1];
-    auto d = res[2];
-    return tuple(x, y - (a / b) * x, d);
+    u %= m;
+    if (u < 0) u += m;
+    return u;
 }
 
 void main()
 {
     int T; get(T);
     while (T--) {
-        long N, S, K; get(N, S, K); K %= N;
-        auto d = gcd(N, K);
-        if (S % d != 0) {
-            writeln(-1);
-            continue;
-        }
+        long N, S, K; get(N, S, K);
+        S = (N - S);
+        auto d = gcd(N, gcd(S, K));
         N /= d;
+        S /= d;
         K /= d;
-        auto r = ext_gcd(N, -K);
-        auto a = r[0] * (S / d);
-        auto b = r[1] * (S / d);
-        if (r[2] > 0) {
-            auto t = min(a / K, b / N);
-            writeln(b - N * t);
-        } else {
-            auto t = max((a + K - 1) / K, (b + N - 1) / N);
-            writeln(N * t - b);
-        }
+        writeln(gcd(K, N) == 1 ? modinv(K, N) * S % N : -1);
     }
 }
 
 /*
 
-S + K * y = N * x
-Nx - Ky = S
-
-
-1 3 1
-
-10x - 3y = 4
-10 * 4 - 3 * 12 = 4
-
-10(x-4) - 3(y-12) = 0
-
-x - 4 = 3t
-x = 4 + 3t
-
-y - 12 = 10t
-y = 12 + 10t
-
-
-t = -1 => x = 1, y = 2
-
-N(x-a) - K(y-b) = 0
-
-x-a = Kt, x = Kt + a, t = a / K
-y-b = Nt, y = Nt + b, t = b / N
-
-10000x - 14y = 6
-10000 * -9 - 14 * -6429 = 6
-
-10000(x+9) - 14(y+6429) = 0
-
-x+9 = 14t, x = 14t - 9, t = 9/14 = 1
-y+6429 = 10000t, y = 10000t - 6429, t = 6429/10000 = 1
-
-1
-85 5 65
-
-
+S + Kx = 0 mod N
+Kx = N - S mod N
 
 */
