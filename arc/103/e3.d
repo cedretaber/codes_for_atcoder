61b11a0715ac1b015f6d8788d0345a07e218489c
cedretaber <cedretaber@gmail.com>
2020-12-20 05:01:59 +0900

    Solve ARC103 e.

---
+++ arc/103/e3.d
@@ -0,0 +1,76 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    char[] s; get(s);
+    if (s[0] == '0' || s[$-1] == '1') return writeln(-1);
+
+    int i, j = s.length.to!int-2;
+    int p = 1, q = 2;
+    int[] as, bs;
+    while (i <= j) {
+        if (s[i] != s[j]) return writeln(-1);
+        as ~= p;
+        bs ~= q;
+        if (s[i] == '1') p = q;
+        ++q; ++i; --j;
+    }
+    for (; q <= s.length; ++q) {
+        as ~= p;
+        bs ~= q;
+    }
+
+    foreach (k; 0..s.length-1) writefln!"%d %d"(as[k], bs[k]);
+}
