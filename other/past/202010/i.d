84d0517fcd09e33d4fc6dae43e5a2123cf106476
cedretaber <cedretaber@gmail.com>
2020-12-24 01:13:15 +0900

    Solve PAST202010 i.

---
+++ other/past/202010/i.d
@@ -0,0 +1,74 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    int N; get(N);
+    long[] aa; get(aa);
+    auto sum_a = reduce!"a + b"(0L, aa);
+    aa = aa ~ aa;
+
+    auto r = abs(sum_a - aa[0] * 2);
+    int i, j = 1;
+    auto s = aa[0];
+    while (i < N*2) {
+        if (j < N*2 && sum_a - s*2 > 0 && j - i < N && j > i) {
+            s += aa[j++];
+        } else {
+            s -= aa[i++];
+        }
+        r = min(r, abs(sum_a - s * 2));
+    }
+    writeln(r);
+}
