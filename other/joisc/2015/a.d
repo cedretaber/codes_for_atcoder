0e2fb3a8ce0f8927456e4939068f725fefc82659
cedretaber <cedretaber@gmail.com>
2020-12-27 02:04:55 +0900

    Solve JOISC2015 a.

---
+++ other/joisc/2015/a.d
@@ -0,0 +1,78 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    int K, M; get(K, M);
+    char[] S; get(S);
+    int N; get(N);
+    int[] AS, BS, CS; get_lines(N, AS, BS, CS);
+
+    auto res = 0.iota(K).array();
+    foreach_reverse (i; 0..N) {
+        auto a = AS[i];
+        auto b = BS[i];
+        auto c = CS[i];
+        auto len = b - a;
+
+        foreach (ref p; res) {
+            if (c <= p && p < c + len) {
+                p = a + p - c;
+            } else if (c + len <= p) {
+                p -= len;
+            }
+        }
+    }
+    writeln(res.map!(p => S[p]).array);
+}
