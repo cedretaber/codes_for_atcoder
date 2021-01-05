3db8d3fa5a30e08adb306ae89452245062b6c075
cedretaber <cedretaber@gmail.com>
2021-01-06 04:21:54 +0900

    Solve PAST202012 d.

---
+++ other/past/202012/d.d
@@ -0,0 +1,73 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+alias N = Tuple!(char[], "s", int, "z", char[], "org");
+
+void main()
+{
+    int n; get(n);
+    N[] ns;
+    foreach (_; 0..n) {
+        char[] org; get(org);
+        auto s = org.dup;
+        int z;
+        while (!s.empty && s.front == '0') {
+            ++z;
+            s.popFront();
+        }
+        ns ~= N(s, z, org);
+    }
+    sort!"a.s.length != b.s.length ? a.s.length < b.s.length : a.s != b.s ? a.s < b.s : a.z > b.z"(ns);
+    foreach (x; ns) writeln(x.org);
+}
