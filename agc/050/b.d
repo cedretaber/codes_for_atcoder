bc5cfb6d430c01723aa4c4f825b76c1f50495b2c
cedretaber <cedretaber@gmail.com>
2021-01-09 05:42:56 +0900

    Solve AGC050 b.

---
+++ agc/050/b.d
@@ -0,0 +1,89 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    int N; get(N);
+    int[] aa; get_lines(N, aa);
+    auto cs = new int[](N + 1);
+    foreach (i; 0..N) cs[i+1] += cs[i] + aa[i];
+
+    auto xs = new int[][](N, N+1);
+    auto ys = new int[][](N, N+1);
+    auto DP1 = new int[](N+1);
+    auto DP2  = new int[](N+1);
+    foreach (k; 3..N+1) foreach (i; 0..N-k+1) {
+        auto j = i + k;
+
+        auto xf = (j - i) % 3 == 0;
+
+        if (xf) {
+            DP1[] = int.min / 3;
+            DP1[i] = cs[j] - cs[i];
+        }
+        DP2[] = 0;
+        foreach (ii; i..j) {
+            if (xf) DP1[ii+1] = max(DP1[ii+1], DP1[ii]);
+            DP2[ii+1] = max(DP2[ii+1], DP2[ii]);
+            for (auto jj = ii+3; jj <= j; jj += 3) {
+                if (ii == i && jj == j) break;
+                if (xf) DP1[jj] = max(DP1[jj], DP1[ii] - (cs[jj] - cs[ii]) + ys[ii][jj]);
+                DP2[jj] = max(DP2[jj], DP2[ii] + xs[ii][jj]);
+            }
+        }
+        if (xf) xs[i][j] = DP1[j];
+        ys[i][j] = DP2[j];
+    }
+    writeln(max(xs[0][N], ys[0][N]));
+}
