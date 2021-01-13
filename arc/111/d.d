9ae9bd774d59024c31d5235cd639062097445e15
cedretaber <cedretaber@gmail.com>
2021-01-14 00:37:15 +0900

    Solve ARC111 d.

---
+++ arc/111/d.d
@@ -0,0 +1,104 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+alias C = Tuple!(int, "i", int, "c");
+
+struct E {
+    int i, to;
+    bool rev;
+
+    bool is_go(int[] ds) {
+        auto d = ds[i];
+        return (d == 1 && !rev) || (d == -1 && rev);
+    }
+
+    @property
+    int back() {
+        return rev ? 1 : -1;
+    }
+}
+
+void main()
+{
+    int N, M; get(N, M);
+    auto G = new E[][N];
+    foreach (i; 0..M) {
+        int a, b; get(a, b); --a; --b;
+        G[a] ~= E(i, b, false);
+        G[b] ~= E(i, a, true);
+    }
+    auto ds = new int[](M);
+    int[] cs; get(cs);
+    C[] cc;
+    foreach (i, c; cs) cc ~= C(i.to!int, c);
+    sort!"a.c < b.c"(cc);
+
+    auto memo = new int[](N);
+    foreach (c; cc) {
+        auto r = 1;
+        foreach (n; G[c.i]) if (n.is_go(ds)) r += cs[n.to];
+        void solve(int i) {
+            foreach (n; G[i]) if (ds[n.i] == 0) {
+                ds[n.i] = n.back;
+                if (cs[n.to] == cs[c.i] && !memo[n.to]) {
+                    memo[n.to] = true;
+                    solve(n.to);
+                }
+            }
+        }
+        memo[c.i] = true;
+        solve(c.i);
+    }
+    writefln!"%-(%s\n%)"(ds.map!(d => d == 1 ? "->" : "<-"));
+}
