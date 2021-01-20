7da737e679bdfc1928241eb474be4fb9c70c1915
cedretaber <cedretaber@gmail.com>
2021-01-21 03:29:58 +0900

    Solve JOI2021yo2 a.

---
+++ other/joi/2021yo2/a.d
@@ -0,0 +1,78 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    int N, A; get(N, A);
+    char[] S; get(S);
+
+    auto rbt = redBlackTree(0, N+1);
+    foreach (i, c; S) if (c == '#') rbt.insert(i.to!int + 1);
+    long res;
+    bool left;
+    while (rbt.length > 2) {
+        if (left) {
+            auto l = rbt.lowerBound(A);
+            res += A - l.back;
+            A = l.back;
+        } else {
+            auto r = rbt.upperBound(A);
+            res += r.front - A;
+            A = r.front;
+        }
+        if (A != 0 && A != N+1) rbt.removeKey(A);
+        left = !left;
+    }
+    writeln(res);
+}
