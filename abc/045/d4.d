4db16f5c589d067fb80a5776196ebcc3b0e36a84
cedretaber <cedretaber@gmail.com>
2021-02-05 00:25:11 +0900

    Solve ABC045 d again.

---
+++ abc/045/d4.d
@@ -0,0 +1,74 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+alias P = Tuple!(int, int);
+
+void main()
+{
+    long H, W; int N; get(H, W, N);
+    int[P] ps;
+    while (N--) {
+        int a, b; get(a, b);
+        static foreach (da; [-1, 0, 1])
+        static foreach (db; [-1, 0, 1]) {{
+            auto i = a + da;
+            auto j = b + db;
+            if (1 < i && i < H && 1 < j && j < W) ++ps[P(i, j)];
+        }}
+    }
+    writeln(H * W - 2*H - 2*W + 4 - ps.length);
+    int[10] res;
+    foreach (_, c; ps) ++res[c];
+    writefln!"%(%d\n%)"(res[1..$]);
+}
