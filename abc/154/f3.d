3da91a4661907d01466f3d0a998465a32f56213a
cedretaber <cedretaber@gmail.com>
2021-02-10 00:44:21 +0900

    Solve ABC154 f again.

---
+++ abc/154/f3.d
@@ -0,0 +1,102 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+
+long P = 10^^9+7;
+long[10^^6 * 2 + 50] F, RF;
+
+long pow(long x, long n) {
+    long y = 1;
+    while (n) {
+        if (n%2 == 1) y = (y * x) % P;
+        x = x^^2 % P;
+        n /= 2;
+    }
+    return y;
+}
+
+long inv(long x)
+{
+    return pow(x, P-2);
+}
+
+void init()
+{
+    F[0] = F[1] = 1;
+    foreach (i, ref x; F[2..$]) x = (F[i+1] * (i+2)) % P;
+
+    {
+        RF[$-1] = 1;
+        auto x = F[$-1];
+        auto k = P-2;
+        while (k) {
+            if (k%2 == 1) RF[$-1] = (RF[$-1] * x) % P;
+            x = x^^2 % P;
+            k /= 2;
+        }
+    }
+    foreach_reverse(i, ref x; RF[0..$-1]) x = (RF[i+1] * (i+1)) % P;
+}
+
+void main()
+{
+    init();
+    long r1, c1, r2, c2; get(r1, c1, r2, c2);
+    long x;
+    foreach (r; r1+1..r2+2) {
+        (x += F[r + c2] * RF[r] % P * RF[c2] % P) %= P;
+        (x += P - F[r + c1 - 1] * RF[r] % P * RF[c1 - 1] % P) %= P;
+    }
+    writeln(x);
+}
