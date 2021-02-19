973ead6b6791bf3fdcc3ebefbfa04bb373f76d10
cedretaber <cedretaber@gmail.com>
2021-02-19 12:00:51 +0900

    Solve pakencamp2020 h.

---
+++ other/pakencamp/2020/h.d
@@ -0,0 +1,94 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    int T; get(T);
+    while (T--) {
+        ulong A, B, C; get(A, B, C);
+        int oe;
+        foreach (i; 0..62) {
+            auto a = !!(A & (1UL<<i));
+            auto b = !!(B & (1UL<<i));
+            auto c = !!(C & (1UL<<i));
+            if (!a && b) goto ng;
+            if (!a && c) goto ng;
+            if (b) {
+                if (oe == 0) {
+                    oe = c ? 1 : 2;
+                } else {
+                    if (c && oe == 2) goto ng;
+                    if (!c && oe == 1) goto ng;
+                }
+            }
+        }
+        writeln("Yes");
+        continue;
+        ng:
+        writeln("No");
+    }
+}
+
+/*
+
+0110
+0110
+0010
+
+0110
+0110
+0110
+
+
+*/
