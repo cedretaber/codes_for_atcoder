a6c69a99216bf163df3fbd0f73d1bf4abb7e382a
cedretaber <cedretaber@gmail.com>
2021-03-03 00:11:48 +0900

    Solve CodeFestival2016qb c again.

---
+++ other/codefestival/2016qb/c3.d
@@ -0,0 +1,112 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    long W, H; get(W, H);
+    long[] ps, qs;
+    get_lines(W, ps);
+    sort(ps);
+    get_lines(H, qs);
+    sort(qs);
+    long r, c = (W + 1) * (H + 1) - 1;
+    ++W; ++H;
+    while (c > 0) {
+        if (qs.empty) {
+            c -= H;
+            r += ps[0] * H;
+            ps.popFront();
+            --W;
+        } else if (ps.empty) {
+            c -= W;
+            r += qs[0] * W;
+            qs.popFront();
+            --H;
+        } else if (ps[0] < qs[0]) {
+            c -= H;
+            r += ps[0] * H;
+            ps.popFront();
+            --W;
+        } else {
+            c -= W;
+            r += qs[0] * W;
+            qs.popFront();
+            --H;
+        }
+    }
+    writeln(r);
+}
+
+/*
+
++-+ +
+| | |
++ +-+
+  | |
++-+ +
+
++-+ +-+-+
+|   |
++-+-+-+-+
+
++-+-+-+-+
+| |   |  
++ +-+ +-+
+
+W * (H + 1) + H = HW + W + H
+
+H * (W + 1) + W = WH + H + W
+=> H(W + 1) + W
+
+*/
