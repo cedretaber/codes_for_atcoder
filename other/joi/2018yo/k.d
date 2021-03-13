69305c5dab7ebe6d33a43013f761de78fc410bb9
cedretaber <cedretaber@gmail.com>
2021-03-14 03:03:21 +0900

    Solve JOI2018yo k.

---
+++ other/joi/2018yo/k.d
@@ -0,0 +1,82 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    int N, K; long L; get(N, K, L);
+    int[] AS; get(AS);
+
+    auto max_a = AS.maxElement();
+    int l, r = max_a;
+    while (l + 1 < r) {
+        auto m = (l + r) / 2;
+
+        long c;
+        int j, x;
+        foreach (i; 0..N) {
+            while (x < K && j < N) {
+                if (AS[j] <= m) ++x;
+                ++j;
+            }
+            if (x >= K) c += N - j + 1;
+            if (AS[i] <= m) --x;
+        }
+        if (c >= L) {
+            r = m;
+        } else {
+            l = m;
+        }
+    }
+    writeln(r);
+}
