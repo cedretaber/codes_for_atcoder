7c26b2f806dae60d52bf65cdacf623d8659e18ec
cedretaber <cedretaber@gmail.com>
2021-03-24 02:34:54 +0900

    Solve iroha day1 k.

---
+++ other/iroha/day1/k.d
@@ -0,0 +1,80 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+enum P = 10L^^9 + 7;
+
+void main()
+{
+    int N; get(N);
+    long res, c = 1;
+    while (N--) {
+        long[] AA; get(AA);
+        auto M = AA[0];
+        AA = AA[1..$];
+
+        long r;
+        foreach (a; AA) {
+            long k, b = a;
+            while (b) {
+                ++k;
+                b /= 10;
+            }
+            (r += res * (10L^^k % P) % P) %= P;
+            (r += a * c % P) %= P;
+        }
+        (c *= M) %= P;
+        res = r;
+    }
+    writeln(res);
+}
