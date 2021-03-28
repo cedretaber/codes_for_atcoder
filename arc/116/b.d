48491c48c167c6b099f699b5ef4744faf5c2c915
cedretaber <cedretaber@gmail.com>
2021-03-29 02:04:23 +0900

    Update ARC116 b.

--- arc/116/b.d
+++ arc/116/b.d
@@ -53,44 +53,9 @@ void get_lines(Args...)(size_t N, ref Args args)
 }
 
 enum P = 998244353L;
-long[10^^5+50] F, RF;
-
-long pow(long x, long n) {
-    long y = 1;
-    while (n) {
-        if (n%2 == 1) y = (y * x) % P;
-        x = x^^2 % P;
-        n /= 2;
-    }
-    return y;
-}
-
-long inv(long x)
-{
-    return pow(x, P-2);
-}
-
-void init()
-{
-    F[0] = F[1] = 1;
-    foreach (i, ref x; F[2..$]) x = (F[i+1] * (i+2)) % P;
-
-    {
-        RF[$-1] = 1;
-        auto x = F[$-1];
-        auto k = P-2;
-        while (k) {
-            if (k%2 == 1) RF[$-1] = (RF[$-1] * x) % P;
-            x = x^^2 % P;
-            k /= 2;
-        }
-    }
-    foreach_reverse(i, ref x; RF[0..$-1]) x = (RF[i+1] * (i+1)) % P;
-}
 
 void main()
 {
-    init();
     int N; get(N);
     long[] AA; get(AA);
     sort(AA);
