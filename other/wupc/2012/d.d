76ff287ab869b25d90dc1eca027f874d7ea32002
cedretaber <cedretaber@gmail.com>
2021-04-16 02:02:23 +0900

    Solve WUPC2012 d.

---
+++ other/wupc/2012/d.d
@@ -0,0 +1,66 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    int N; get(N);
+    int[][] AA; get_lines(N, AA);
+
+    auto DP = new int[][](N + 1, N + 1);
+    foreach (i; 0..N) foreach (j; 0..AA[i].length) {
+        DP[i + 1][j] = max(DP[i + 1][j], DP[i][j] + AA[i][j]);
+        DP[i + 1][j + 1] = max(DP[i + 1][j + 1], DP[i][j] + AA[i][j]);
+    }
+    writeln(DP[N].maxElement());
+}
