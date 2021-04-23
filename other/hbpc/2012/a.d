3c1a368df7c66be786b64bd566fad06cc8bb2edd
cedretaber <cedretaber@gmail.com>
2021-04-24 01:25:20 +0900

    Solve hbpc2012 a.

---
+++ other/hbpc/2012/a.d
@@ -0,0 +1,77 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    int M, N; get(M, N);
+    int[int] rs;
+    while (M--) {
+        int A, B; get(A, B);
+        auto k = B - A;
+        if (k !in rs) rs[k] = A;
+        rs[k] = min(rs[k], A);
+    }
+    auto DP = new int[](20001);
+    DP[] = -1; DP[1] = 1;
+    alias S = Tuple!(int, "i", int, "n");
+    auto ss = [S(1, 1)];
+    while (!ss.empty) with (ss.front) {
+        ss.popFront();
+        foreach (d, a; rs) if (i >= a && i + d > 0 && i + d <= 20000 && DP[i + d] == -1) {
+            DP[i + d] = n + 1;
+            ss ~= S(i + d, n + 1);
+        }
+    }
+    writeln(DP[N]);
+}
