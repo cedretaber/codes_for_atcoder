31dd68e99995b3809da180cfe83675a665a9bc35
cedretaber <cedretaber@gmail.com>
2021-04-25 11:06:14 +0900

    Solve petrozavodsk d again.

---
+++ other/petrozavodsk/d4.d
@@ -0,0 +1,154 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+struct Dsu
+{
+public:
+    this(int n)
+    {
+        _n = n, parent_or_size = new int[](n);
+        parent_or_size[] = -1;
+    }
+
+    int merge(int a, int b)
+    {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        int x = leader(a), y = leader(b);
+        if (x == y)
+            return x;
+        if (-parent_or_size[x] < -parent_or_size[y])
+        {
+            auto tmp = x;
+            x = y;
+            y = tmp;
+        }
+        parent_or_size[x] += parent_or_size[y];
+        parent_or_size[y] = x;
+        return x;
+    }
+
+    bool same(int a, int b)
+    {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        return leader(a) == leader(b);
+    }
+
+    int leader(int a)
+    {
+        assert(0 <= a && a < _n);
+        if (parent_or_size[a] < 0)
+            return a;
+        return parent_or_size[a] = leader(parent_or_size[a]);
+    }
+
+    int size(int a)
+    {
+        assert(0 <= a && a < _n);
+        return -parent_or_size[leader(a)];
+    }
+
+    int[][] groups()
+    {
+        auto leader_buf = new int[](_n), group_size = new int[](_n);
+        foreach (i; 0 .. _n)
+        {
+            leader_buf[i] = leader(i);
+            group_size[leader_buf[i]]++;
+        }
+        auto result = new int[][](_n);
+        foreach (i; 0 .. _n)
+            result[i].reserve(group_size[i]);
+        foreach (i; 0 .. _n)
+            result[leader_buf[i]] ~= i;
+        int[][] filtered;
+        foreach (r; result)
+            if (r.length != 0)
+                filtered ~= r;
+        return filtered;
+    }
+
+private:
+    int _n;
+    int[] parent_or_size;
+}
+
+void main()
+{
+    int N, M; get(N, M);
+    long[] AA; get(AA);
+    auto uft = Dsu(N);
+    while (M--) {
+        int X, Y; get(X, Y);
+        uft.merge(X, Y);
+    }
+    long[][int] gs;
+    foreach (i; 0..N) gs[uft.leader(i)] ~= AA[i];
+    if (gs.length == 1) return writeln(0);
+    long[] rs;
+    long r;
+    foreach (_, aa; gs) {
+        sort(aa);
+        r += aa[0];
+        if (aa.length > 1) rs ~= aa[1..$];
+    }
+    auto c = (gs.length - 1).to!int * 2 - gs.length.to!int;
+    if (rs.length < c) {
+        writeln("Impossible");
+    } else {
+        sort(rs);
+        writeln(r + rs[0..c].sum);
+    }
+}
