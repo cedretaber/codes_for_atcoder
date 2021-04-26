5559de39e4a1a90ceda4fc7a915858d6b4393ce4
cedretaber <cedretaber@gmail.com>
2021-04-27 03:42:16 +0900

    Solve tenkaichi2012qb a.

---
+++ other/tenkaichi/2012qb/a.d
@@ -0,0 +1,231 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+import std.typecons : Tuple;
+
+ulong safeMod(long x, long m)
+{
+    x %= m;
+    if (x < 0)
+        x += m;
+    return x;
+}
+
+long ctPowMod(long x, long n, int m)
+{
+    if (m == 1)
+        return 0;
+    uint _m = cast(uint) m;
+    ulong r = 1;
+    ulong y = safeMod(x, m);
+    while (n)
+    {
+        if (n & 1)
+            r = (r * y) % _m;
+        y = (y * y) % _m;
+        n >>= 1;
+    }
+    return r;
+}
+
+bool ctIsPrime(int n)
+{
+    if (n <= 1)
+        return false;
+    if (n == 2 || n == 7 || n == 61)
+        return true;
+    if (n % 2 == 0)
+        return false;
+    long d = n - 1;
+    while (d % 2 == 0)
+        d /= 2;
+    foreach (a; [2, 7, 61])
+    {
+        long t = d;
+        long y = ctPowMod(a, t, n);
+        while (t != n - 1 && y != 1 && y != n - 1)
+        {
+            y = y * y % n;
+            t <<= 1;
+        }
+        if (y != n - 1 && t % 2 == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+enum bool isPrime(int n) = ctIsPrime(n);
+
+Tuple!(long, long) invGcd(long a, long b)
+{
+    a = safeMod(a, b);
+    if (a == 0)
+        return Tuple!(long, long)(b, 0);
+    long s = b, t = a, m0 = 0, m1 = 1;
+    while (t)
+    {
+        long u = s / t;
+        s -= t * u;
+        m0 -= m1 * u;
+        long tmp = s;
+        s = t;
+        t = tmp;
+        tmp = m0;
+        m0 = m1;
+        m1 = tmp;
+    }
+    if (m0 < 0)
+        m0 += b / s;
+    return Tuple!(long, long)(s, m0);
+}
+
+int ctPrimitiveRoot(int m)
+{
+    if (m == 2)
+        return 1;
+    if (m == 167_772_161)
+        return 3;
+    if (m == 469_762_049)
+        return 3;
+    if (m == 754_974_721)
+        return 11;
+    if (m == 998_244_353)
+        return 3;
+    int[20] divs;
+    divs[0] = 2;
+    int cnt = 1;
+    int x = (m - 1) / 2;
+    while (x % 2 == 0)
+        x /= 2;
+    for (int i = 3; (cast(long) i) * i <= x; i += 2)
+        if (x % i == 0)
+        {
+            divs[cnt++] = i;
+            while (x % i == 0)
+                x /= i;
+        }
+    if (x > 1)
+        divs[cnt++] = x;
+    for (int g = 2;; g++)
+    {
+        bool ok = true;
+        foreach (i; 0 .. cnt)
+            if (ctPowMod(g, (m - 1) / divs[i], m) == 1)
+            {
+                ok = false;
+                break;
+            }
+        if (ok)
+            return g;
+    }
+}
+
+enum primitiveRoot(int m) = ctPrimitiveRoot(m);
+
+Tuple!(long, long) crt(long[] r, long[] m)
+{
+    assert(r.length == m.length);
+    long r0 = 0, m0 = 1;
+    foreach (i; 0 .. r.length)
+    {
+        assert(1 <= m[i]);
+        long r1 = safeMod(r[i], m[i]);
+        long m1 = m[i];
+        if (m0 < m1)
+        {
+            auto tmp = r0;
+            r0 = r1;
+            r1 = tmp;
+            tmp = m0;
+            m0 = m1;
+            m1 = tmp;
+        }
+        if (m0 % m1 == 0)
+        {
+            if (r0 % m1 != r1)
+                return Tuple!(long, long)(0, 0);
+            continue;
+        }
+        long g, im;
+        {
+            auto tmp = invGcd(m0, m1);
+            g = tmp[0];
+            im = tmp[1];
+        }
+        long u1 = m1 / g;
+        if ((r1 - r0) % g)
+            return Tuple!(long, long)(0, 0);
+        long x = (r1 - r0) / g % u1 * im % u1;
+        r0 += x * m0;
+        m0 *= u1;
+        if (r0 < 0)
+            r0 += m0;
+    }
+    return Tuple!(long, long)(r0, m0);
+}
+
+void main()
+{
+    long[] abc; get(abc);
+    auto yz = crt(abc, [3, 5, 7]);
+    auto y = yz[0];
+    auto z = yz[1];
+    if (z == 0) return;
+    while (y < 128) {
+        if (y) writeln(y);
+        y += z;
+    }
+}
