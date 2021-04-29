8d709f1c11de722969f38be61479e3c1fa83344e
cedretaber <cedretaber@gmail.com>
2021-04-30 00:26:53 +0900

    Solve UTPC2011 c.

---
+++ other/utpc/2011/c.d
@@ -0,0 +1,90 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+enum R = [
+    '(': ')',
+    ')': '(',
+    '{': '}',
+    '}': '{',
+    '[': ']',
+    ']': '['
+];
+
+void main()
+{
+    char[] S; get(S);
+    char[] ll, rr;
+    foreach (i, c; S) {
+        if (c != 'i') {
+            ll ~= c;
+        } else {
+            if (i + 3 < S.length) {
+                rr = S[i + 3..$];
+                rr.reverse();
+            }
+            break;
+        }
+    }
+    int solve(int i, int j) {
+        if (i >= ll.length || j >= rr.length) {
+            return 0;
+        } else {
+            auto r = solve(i + 1, j);
+            while (j < rr.length && R[ll[i]] != rr[j]) ++j;
+            if (j < rr.length) r = max(r, solve(i + 1, j + 1) + 2);
+            return r;
+        }
+    }
+    writeln(solve(0, 0) + 3);
+}
