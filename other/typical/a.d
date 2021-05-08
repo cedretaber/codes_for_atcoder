c366cfd111bc5a451f0b6c47a467d71b3c26a92c
cedretaber <cedretaber@gmail.com>
2021-05-09 02:24:30 +0900

    Solve Typical90 a to d.

---
+++ other/typical/a.d
@@ -0,0 +1,85 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    int N, L; get(N, L);
+    int K; get(K);
+    int[] yy;
+    int b;
+    foreach (a; readln.split.to!(int[])) {
+        yy ~= a - b;
+        b = a;
+    }
+    yy ~= L - b;
+    int l, r = L + 1;
+    while (l + 1 < r) {
+        auto m = (l + r) / 2;
+        int v, k = -1;
+        foreach (y; yy) {
+            if (v + y >= m) {
+                ++k;
+                v = 0;
+            } else {
+                v += y;
+            }
+        }
+        if (k >= K) {
+            l = m;
+        } else {
+            r = m;
+        }
+    }
+    writeln(l);
+}
