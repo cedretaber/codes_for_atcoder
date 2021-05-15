24422d36018bcb07ddca6c81f808882a665a365c
cedretaber <cedretaber@gmail.com>
2021-05-16 02:32:33 +0900

    Solve KUPC2012 d.

---
+++ other/kupc/2012/d.d
@@ -0,0 +1,71 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    int N, M; get(N, M);
+    alias P = Tuple!(int, "l", int, "r");
+    P[] ps; get_lines(M, ps);
+    sort!"a.l < b.l"(ps);
+    auto DP = new int[][](N + 1, M + 1);
+    foreach (ref dp; DP) dp[] = int.max / 3;
+    DP[0][0] = 0;
+    foreach (i; 0..N + 1) foreach (j; 0..M) {
+        DP[i][j + 1] = min(DP[i][j + 1], DP[i][j]);
+        auto p = ps[j];
+        if (p.l <= i + 1 && i < p.r) DP[p.r][j + 1] = min(DP[p.r][j + 1], DP[i][j] + 1);
+    }
+    auto r = DP[N].minElement();
+    writeln(r >= int.max / 3 ? "Impossible" : r.to!string);
+}
