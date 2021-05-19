e94036f41b4db2749a54e4e78cdf6000fcdc4c12
cedretaber <cedretaber@gmail.com>
2021-05-20 01:43:21 +0900

    Solve k2pc b.

---
+++ other/k2pc/easy/b.d
@@ -0,0 +1,77 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    int N; get(N);
+    int[] aa; get(aa);
+    sort!"a > b"(aa);
+
+    int[7] cs, ds, ps;
+    foreach (_; 0..N) {
+        char[] CC; get(CC);
+        foreach (i; 0..7) {
+            if (CC[i] == 'X') {
+                ++ds[i];
+            } else {
+                cs[i] = max(cs[i], ds[i]);
+                ds[i] = 0;
+            }
+        }
+    }
+    foreach (i; 0..7) cs[i] = max(cs[i], ds[i]);
+    sort!"a > b"(cs[]);
+    foreach (i; 0..7) if (cs[i] > aa[i]) return writeln("NO");
+    writeln("YES");
+}
