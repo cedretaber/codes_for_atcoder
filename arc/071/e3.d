a0cb391357465f07ce4ad1247a711f3e655cde26
cedretaber <cedretaber@gmail.com>
2021-05-21 00:09:43 +0900

    Solve ARC071 e again.

---
+++ arc/071/e3.d
@@ -0,0 +1,67 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    char[] S, T; get(S); get(T);
+    auto ss = new int[](S.length + 1);
+    foreach (i, c; S) ss[i + 1] += ss[i] + ((c == 'A') ? 1 : 2);
+    auto tt = new int[](T.length + 1);
+    foreach (i, c; T) tt[i + 1] += tt[i] + ((c == 'A') ? 1 : 2);
+
+    int Q; get(Q); while (Q--) {
+        int a, b, c, d; get(a, b, c, d);
+        writeln(((ss[b] - ss[a - 1]) % 3) == ((tt[d] - tt[c - 1]) % 3) ? "YES" : "NO");
+    }
+}
