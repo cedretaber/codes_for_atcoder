060d5fe32a177e03bf6021f7673373fdb8731140
cedretaber <cedretaber@gmail.com>
2021-05-21 03:39:47 +0900

    Solve Typical90 n and o.

---
+++ other/typical/o.d
@@ -0,0 +1,130 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+enum P = 10L^^9 + 7;
+long[10^^5 * 2 + 50] F, RF;
+
+void init()
+{
+    F[0] = F[1] = 1;
+    foreach (i, ref x; F[2..$]) x = (F[i+1] * (i+2)) % P;
+
+    {
+        RF[$-1] = 1;
+        auto x = F[$-1];
+        auto k = P-2;
+        while (k) {
+            if (k%2 == 1) RF[$-1] = (RF[$-1] * x) % P;
+            x = x^^2 % P;
+            k /= 2;
+        }
+    }
+    foreach_reverse(i, ref x; RF[0..$-1]) x = (RF[i+1] * (i+1)) % P;
+}
+
+long comb(N)(N n, N k)
+{
+    if (k > n) return 0;
+    auto n_b = F[n];    // n!
+    auto nk_b = RF[n-k]; // 1 / (n-k)!
+    auto k_b = RF[k];    // 1 / k!
+
+    auto nk_b_k_b = (nk_b * k_b) % P; // 1 / (n-k)!k!
+
+    return (n_b * nk_b_k_b) % P;  // n! / (n-k)!k!
+}
+
+void main()
+{
+    init();
+    long N; get(N);
+    foreach (k; 1..N + 1) {
+        long r;
+        foreach (a; 1..(N + k - 1) / k + 1) {
+            (r += comb(N - (k - 1) * (a - 1), a)) %= P;
+        }
+        writeln(r);
+    }
+}
+
+/*
+
+16 8 4 2
+
+8 5 3 2
+
+6 4 3 2
+
+5 4 3 2
+
+128 64 32 16 8 4 2
+34 21 13 8 5 3 2
+19 13 9 6 4 3 2
+14 10 7 5 4 3 2
+11 8 6 5 4 3 2
+9 7 6 5 4 3 2
+8 7 6 5 4 3 2
+
+
+128 64 32 16 8 4
+34 21 13 8 5
+19 13 9 6
+14 10 7
+11 8
+9 
+8 7 6 5 4 3 2
+
+1 2 3 5 15 94
+
+*/
