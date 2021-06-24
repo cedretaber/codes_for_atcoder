d8e8b0c9fbfe575765091045a1264d6deab7118e
cedretaber <cedretaber@gmail.com>
2021-06-25 03:53:36 +0900

    Solve AutumnFest001 e.

---
+++ other/autumn_fest/001/e.d
@@ -0,0 +1,72 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    int N; get(N);
+    alias P = Tuple!(long, "x", long, "y");
+    P[] ps; get_lines(N, ps);
+    auto t = abs(ps[0].x + ps[0].y) % 2;
+    auto max_s = abs(ps[0].x) + abs(ps[0].y);
+    foreach (p; ps) {
+        if (t != abs(p.x + p.y) % 2) return writeln(-1);
+        max_s = max(max_s, abs(p.x) + abs(p.y));
+    }
+    long r;
+    for (;;) {
+        auto n = r * (r + 1) / 2;
+        if (n >= max_s && n % 2 == t) return writeln(r);
+        ++r;
+    }
+}
