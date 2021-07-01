92f1781e1b548d8b43e4cf2ff0effbb7ea1d8e1a
cedretaber <cedretaber@gmail.com>
2021-07-01 22:12:51 +0900

    Solve ABC058 d again.

---
+++ abc/058/d5.d
@@ -0,0 +1,70 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+enum P = 10L^^9 + 7;
+
+void main()
+{
+    long N, M; get(N, M);
+    long[] XX, YY; get(XX); get(YY);
+    long[] xs, ys;
+    foreach (i; 1..N) xs ~= XX[i] - XX[i - 1];
+    foreach (i; 1..M) ys ~= YY[i] - YY[i - 1];
+
+    long x;
+    foreach (i; 0..N - 1) (x += (i + 1) * (N - i - 1) % P * xs[i] % P) %= P;
+    long r;
+    foreach (i; 0..M - 1) (r += (i + 1) * (M - i - 1) % P * x % P * ys[i] % P) %= P;
+    writeln(r);
+}
