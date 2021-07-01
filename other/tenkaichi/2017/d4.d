677d44d18e667b9e449594e06cd2ad1f7a25f7d2
cedretaber <cedretaber@gmail.com>
2021-07-02 03:16:41 +0900

    Solve tenkaichi2017 d again.

---
+++ other/tenkaichi/2017/d4.d
@@ -0,0 +1,68 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    int N; uint K; get(N, K);
+    uint[] AS; long[] BS; get_lines(N, AS, BS);
+
+    long r;
+    foreach (x; 0..31) if (x == 30 || (K & (1<<x))) {
+        auto k = x == 30 ? K : K ^ (1<<x) | ((1<<x)-1);
+        long s;
+        foreach (i; 0..N) if ((AS[i] | k) == k) s += BS[i];
+        r = max(r, s);
+    }
+    writeln(r);
+}
