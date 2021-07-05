863f32880492ac4bfbad8ed05845be1d85f70aa8
cedretaber <cedretaber@gmail.com>
2021-07-06 02:11:05 +0900

    Solve ARC122 a.

---
+++ arc/122/a.d
@@ -0,0 +1,86 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+enum P = 10L^^9 + 7;
+
+void main()
+{
+    int N; get(N);
+    long[] AA; get(AA);
+    auto DP = new long[][](N, 2);
+    DP[0][0] = 1;
+    foreach (i; 1..N) {
+        (DP[i][0] += DP[i - 1][0] + DP[i - 1][1]) %= P;
+        (DP[i][1] += DP[i - 1][0]) %= P;
+    }
+    long res, all = (DP[N - 1][0] + DP[N - 1][1]) % P;
+    foreach (i; 0..N) {
+        if (i == 0) {
+            (res += all * AA[i] % P) %= P;
+        } else if (i == N - 1) {
+            (res += (DP[N - 1][0] + P - DP[N - 1][1]) % P * AA[i] % P) %= P;
+        } else {
+            (res += (all + P - DP[i - 1][0] * DP[N - i - 1][0] % P * 2 % P) % P * AA[i] % P) %= P;
+        }
+    }
+    writeln(res);
+}
+
+/*
+
+1 * 1 -1
+1 * 1 1
+1 * -1 1
+
+*/
