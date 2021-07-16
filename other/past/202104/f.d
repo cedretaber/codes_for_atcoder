e95c8869e5bdd5da97adef97ad5b9fac6eb17336
cedretaber <cedretaber@gmail.com>
2021-07-16 12:44:09 +0900

    Solve PAST202104 f.

---
+++ other/past/202104/f.d
@@ -0,0 +1,79 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    int N, L, T, X; get(N, L, T, X);
+    int res, p;
+    while (N--) {
+        int A, B; get(A, B);
+        if (B < L) {
+            p = 0;
+            res += A;
+        } else if (p + A < T) {
+            p += A;
+            res += A;
+        } else if (A <= T) {
+            res += X + A + (p + A > T ? T - p : 0);
+            p = p + A > T ? A : 0;
+            if (p == T) {
+                res += X;
+                p = 0;
+            }
+        } else {
+            return writeln("forever");
+        }
+    }
+    writeln(res);
+}
