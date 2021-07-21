c57423368497d48722d9c77219820de17928ba1f
cedretaber <cedretaber@gmail.com>
2021-07-22 03:04:23 +0900

    Solve JSC2019q c again.

---
+++ other/jsc/2019q/c4.d
@@ -0,0 +1,74 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+enum P = 10L ^^ 9 + 7;
+
+void main()
+{
+    long N; get(N);
+    char[] S; get(S);
+
+    long r = 1, x;
+    foreach (i; 2..N + 1) (r *= i) %= P;
+    foreach (c; S) {
+        if ((x % 2 == 0 && c == 'B') || (x % 2 == 1 && c == 'W')) {
+            ++x;
+        } else {
+            (r *= x) %= P;
+            --x;
+        }
+    }
+    if (x) r = 0;
+    writeln(r);
+}
