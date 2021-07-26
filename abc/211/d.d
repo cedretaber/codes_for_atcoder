57283c8f1a9c7b7ac139a0d81c211f2fc0b92ed7
cedretaber <cedretaber@gmail.com>
2021-07-26 23:27:51 +0900

    Solve ABC211 d.

---
+++ abc/211/d.d
@@ -0,0 +1,87 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+enum P = 10L^^9 + 7;
+
+void main()
+{
+    int N, M; get(N, M);
+    auto G = new int[][N];
+    foreach (_; 0..M) {
+        int A, B; get(A, B); --A; --B;
+        G[A] ~= B;
+        G[B] ~= A;
+    }
+
+    auto memo = new int[](N);
+    memo[] = int.max;
+    auto DP = new long[](N);
+    DP[0] = 1;
+    auto Q = [0];
+    int t;
+    while (!Q.empty) {
+        ++t;
+        int[] qq;
+        foreach (i; Q) foreach (j; G[i]) if (memo[j] >= t) {
+            (DP[j] += DP[i]) %= P;
+            if (memo[j] > t) {
+                qq ~= j;
+                memo[j] = t;
+            }
+        }
+        Q = qq;
+    }
+
+    writeln(DP[N - 1]);
+}
