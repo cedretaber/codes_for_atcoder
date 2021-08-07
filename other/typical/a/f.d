fbeca700e0dbfa8d3bea0ca58fd6f4a6d4237cb7
cedretaber <cedretaber@gmail.com>
2021-08-07 20:53:45 +0900

    Solve Typical90 af.

---
+++ other/typical/a/f.d
@@ -0,0 +1,76 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    int N; get(N);
+    int[][] AA; get_lines(N, AA);
+    int M; get(M);
+    auto bs = new bool[][](N, N);
+    while (M--) {
+        int X, Y; get(X, Y); --X; --Y;
+        bs[X][Y] = bs[Y][X] = true;
+    }
+
+    auto rs = 0.iota(N).array;
+    int res = int.max;
+    loop: do {
+        foreach (i; 0..N - 1) if (bs[rs[i]][rs[i + 1]]) continue loop;
+        int r;
+        foreach (j, i; rs) r += AA[i][j];
+        res = min(res, r);
+    } while (nextPermutation(rs));
+
+    writeln(res == int.max ? -1 : res);
+}
