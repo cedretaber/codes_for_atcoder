08225d8cb0c8052e483ea5045e3a686ba74a1aca
cedretaber <cedretaber@gmail.com>
2021-08-12 05:13:22 +0900

    Update

---
+++ abc/013/d4.d
@@ -0,0 +1,71 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    int N, M; long D; get(N, M, D);
+    auto aa = new int[][](N, 33);
+    foreach (i; 0..N) {
+        aa[i][0] = i;
+    }
+    foreach_reverse (a; readln.split.to!(int[])) swap(aa[a-1][0], aa[a][0]);
+    foreach (j; 1..33) foreach (i; 0..N) {
+        aa[i][j] = aa[aa[i][j-1]][j-1];
+    }
+    auto rs = 0.iota(N).array;
+    foreach_reverse (x; 0..32) if (D & (1L << x)) {
+        foreach (ref r; rs) r = aa[r][x];
+    }
+    writefln!"%(%d\n%)"(rs.map!"a + 1");
+}
