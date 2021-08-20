a05bc9b8056aaea8b84f404ef2ae0d8fdced1801
cedretaber <cedretaber@gmail.com>
2021-08-21 07:55:11 +0900

    Solve ABC214 f.

---
+++ abc/214/f.d
@@ -0,0 +1,71 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+enum P = 10L ^^ 9 + 7;
+
+void main()
+{
+    char[] S; get(S);
+    long N = S.length;
+
+    auto DP = new long[](N + 2);
+    DP[0] = 1;
+    foreach (i; 2..N + 2) {
+        foreach_reverse (j; 0..i) {
+            (DP[i] += DP[j - 1]) %= P;
+            if (j == 1 || S[i - 2] == S[j - 2]) break;
+        }
+    }
+    writeln(DP[2..$].sum() % P);
+}
