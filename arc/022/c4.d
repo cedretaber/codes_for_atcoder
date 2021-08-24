022963ff54f8ce91f0b6e0ec12751d175caac623
cedretaber <cedretaber@gmail.com>
2021-08-24 23:48:25 +0900

    Solve ARC022 c again.

---
+++ arc/022/c4.d
@@ -0,0 +1,78 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    int N; get(N);
+    auto T = new int[][N];
+    foreach (_; 1..N) {
+        int A, B; get(A, B); --A; --B;
+        T[A] ~= B;
+        T[B] ~= A;
+    }
+
+    int x, max_d;
+    void walk(int i, int p, int d) {
+        if (d > max_d) {
+            max_d = d;
+            x = i;
+        }
+        foreach (j; T[i]) if (j != p) walk(j, i, d + 1);
+    }
+    walk(0, 0, 0);
+    auto y = x;
+    max_d = 0;
+    walk(y, y, 0);
+    writeln(x + 1, " ", y + 1);
+}
