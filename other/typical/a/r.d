500f730c961cd56c817cf8fcbc58deab87e199b6
cedretaber <cedretaber@gmail.com>
2021-09-04 21:04:29 +0900

    Solve Typical90 ar.

---
+++ other/typical/a/r.d
@@ -0,0 +1,74 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    int N, Q; get(N, Q);
+    int[] AA; get(AA);
+    AA.reverse();
+    AA.reserve(N * 2);
+
+    while (Q--) {
+        int T, x, y; get(T, x, y);
+        if (T == 1) {
+            swap(AA[N - x], AA[N - y]);
+        } else if (T == 2) {
+            auto a = AA.front;
+            AA.popFront();
+            AA ~= a;
+        } else {
+            writeln(AA[N - x]);
+        }
+    }
+}
