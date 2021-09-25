4421e3e104ec7f42aa12292ce97d8986833d3138
cedretaber <cedretaber@gmail.com>
2021-09-26 05:26:44 +0900

    Solve ARC127 a.

---
+++ arc/127/a.d
@@ -0,0 +1,80 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    char[] N; get(N);
+    long r;
+    foreach (i; 0..N.length) {
+        bool u = true;
+        foreach (j; i..N.length) {
+            if (i == 0 && u && N[j] == '0') break;
+            if (N[j] != '1') u = false;
+            long n;
+            if (i == 0 && u) {
+                foreach (k; j + 1 .. N.length) {
+                    n *= 10;
+                    n += N[k] - '0';
+                }
+            } else {
+                foreach (k; j + 1 .. N.length) {
+                    n *= 10;
+                    n += 9;
+                }
+            }
+            r += n + 1;
+        }
+    }
+    writeln(r);
+}
