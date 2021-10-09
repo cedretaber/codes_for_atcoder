117454ef958348b030864d724b35a5795a3f7d90
cedretaber <cedretaber@gmail.com>
2021-10-09 23:39:08 +0900

    Solve Typical90 bf.

---
+++ other/typical/b/f.d
@@ -0,0 +1,80 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+enum P = 10L ^^ 5;
+
+void main()
+{
+    long N, K; get(N, K);
+
+    auto nn = new long[][](P, 63);
+    foreach (i; 0..P) {
+        auto x = i;
+        long n = i;
+        while (x) {
+            n += x % 10;
+            x /= 10;
+        }
+        nn[i][0] = n % P;
+    }
+    foreach (j; 1..63) foreach (i; 0..P) {
+        nn[i][j] = nn[nn[i][j - 1]][j - 1];
+    }
+
+    foreach (j; 0..63) if (K & (1L << j)) {
+        N = nn[N][j];
+    }
+
+    writeln(N);
+}
