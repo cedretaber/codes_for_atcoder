3d2ec3b577a2e58d6b33db453373327ec0b39a8f
cedretaber <cedretaber@gmail.com>
2021-10-21 22:41:48 +0900

    Solve Typical90 bl.

---
+++ other/typical/b/l.d
@@ -0,0 +1,81 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric, std.container, std.range;
+
+void get(Args...)(ref Args args)
+{
+    import std.traits, std.meta, std.typecons;
+
+    static if (Args.length == 1) {
+        alias Arg = Args[0];
+        
+        static if (isArray!Arg) {
+          static if (isSomeChar!(ElementType!Arg)) {
+            args[0] = readln.chomp.to!Arg;
+          } else {
+            args[0] = readln.split.to!Arg;
+          }
+        } else static if (isTuple!Arg) {
+            auto input = readln.split;
+            static foreach (i; 0..Fields!Arg.length) {
+                args[0][i] = input[i].to!(Fields!Arg[i]);
+            }
+        } else {
+            args[0] = readln.chomp.to!Arg;
+        }
+    } else {
+        auto input = readln.split;
+        assert(input.length == Args.length);
+
+        static foreach (i; 0..Args.length) {
+            args[i] = input[i].to!(Args[i]);
+        }
+    }
+}
+
+void get_lines(Args...)(size_t N, ref Args args)
+{
+    import std.traits, std.range;
+
+    static foreach (i; 0..Args.length) {
+        static assert(isArray!(Args[i]));
+        args[i].length = N;
+    }
+
+    foreach (i; 0..N) {
+        static if (Args.length == 1) {
+            get(args[0][i]);
+        } else {
+            auto input = readln.split;
+            static foreach (j; 0..Args.length) {
+                args[j][i] = input[j].to!(ElementType!(Args[j]));
+            }
+        }
+    }
+}
+
+void main()
+{
+    int N, Q; get(N, Q);
+    long[] AA; get(AA);
+
+    long[] ds;
+    long r;
+    foreach (i; 1..N) {
+        ds ~= AA[i - 1] - AA[i];
+        r += abs(ds[$ - 1]);
+    }
+
+    while (Q--) {
+        int L, R; long V; get(L, R, V);
+        if (L != 1) {
+            r -= abs(ds[L - 2]);
+            ds[L - 2] -= V;
+            r += abs(ds[L - 2]);
+        }
+        if (R != N) {
+            r -= abs(ds[R - 1]);
+            ds[R - 1] += V;
+            r += abs(ds[R - 1]);
+        }
+        writeln(r);
+    }
+}
